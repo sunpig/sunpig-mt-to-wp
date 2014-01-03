9f8691312b3edbd2eef97e8333722769c24f933c
Martin Sutherland <martin@sunpig.com>
2014-01-03 21:15:08 +0100

    Add procedure for copying categories

--- procedures.sql
+++ procedures.sql
@@ -1,5 +1,105 @@
 delimiter //
 
+DROP PROCEDURE IF EXISTS copy_mt_categories_to_wp_terms //
+
+CREATE PROCEDURE copy_mt_categories_to_wp_terms(mt_blog_id INT, wp_blog_id INT)
+BEGIN
+	SET @wp_table_infix = if(wp_blog_id = 1, '', CONCAT(wp_blog_id, '_'));
+
+	SET @str_truncate_wp_table = CONCAT('TRUNCATE TABLE wp_', @wp_table_infix, 'term_relationships');
+	PREPARE stmt_truncate_wp_table FROM @str_truncate_wp_table;
+	EXECUTE stmt_truncate_wp_table;
+	DEALLOCATE PREPARE stmt_truncate_wp_table;
+
+	SET @str_truncate_wp_table = CONCAT('TRUNCATE TABLE wp_', @wp_table_infix, 'term_taxonomy');
+	PREPARE stmt_truncate_wp_table FROM @str_truncate_wp_table;
+	EXECUTE stmt_truncate_wp_table;
+	DEALLOCATE PREPARE stmt_truncate_wp_table;
+
+	SET @str_truncate_wp_table = CONCAT('TRUNCATE TABLE wp_', @wp_table_infix, 'terms');
+	PREPARE stmt_truncate_wp_table FROM @str_truncate_wp_table;
+	EXECUTE stmt_truncate_wp_table;
+	DEALLOCATE PREPARE stmt_truncate_wp_table;
+
+	SET @str_copy_categories = CONCAT(
+		'INSERT INTO wp_', @wp_table_infix, 'terms (',
+			'term_id, ',
+			'name, ',
+			'slug, ',
+			'term_group ',
+		') ',
+		' SELECT ',
+			'category_id, ',
+			'category_label, ',
+			'category_basename, ',
+			'0 '
+		' FROM mt_category ',
+		' WHERE category_blog_id = ', mt_blog_id, ' ORDER BY category_id ASC');
+	PREPARE stmt_copy_categories FROM @str_copy_categories;
+	EXECUTE stmt_copy_categories;
+	DEALLOCATE PREPARE stmt_copy_categories;
+
+	SET @str_create_term_taxonomy = CONCAT(
+		'INSERT INTO wp_', @wp_table_infix, 'term_taxonomy (',
+			'term_id, ',
+			'taxonomy, ',
+			'description, ',
+			'parent, ',
+			'count',
+		') ',
+		' SELECT ',
+			'category_id, ',
+			'\'category\', ',
+			'\'\', ',
+			'0, '
+			'0 '
+		' FROM mt_category ',
+		' WHERE category_blog_id = ', mt_blog_id, ' ORDER BY category_id ASC');
+	PREPARE stmt_create_term_taxonomy FROM @str_create_term_taxonomy;
+	EXECUTE stmt_create_term_taxonomy;
+	DEALLOCATE PREPARE stmt_create_term_taxonomy;
+
+	SET @str_create_term_relationships = CONCAT(
+		'INSERT INTO wp_', @wp_table_infix, 'term_relationships (',
+			'object_id, ',
+			'term_taxonomy_id, ',
+			'term_order ',
+		') ',
+
+		' select ',
+			' mt_placement.placement_entry_id, ',
+			' wp_', @wp_table_infix, 'term_taxonomy.term_taxonomy_id, ',
+			' 1 ',
+		' from ',
+			' mt_placement ',
+			' inner join wp_', @wp_table_infix, 'term_taxonomy on mt_placement.placement_category_id = wp_', @wp_table_infix, 'term_taxonomy.term_id ',
+		' where  ',
+			' mt_placement.placement_blog_id = ', mt_blog_id, ' ',
+			' and wp_', @wp_table_infix, 'term_taxonomy.taxonomy = \'category\' ',
+		' order by mt_placement.placement_entry_id; ');
+	PREPARE stmt_create_term_relationships FROM @str_create_term_relationships;
+	EXECUTE stmt_create_term_relationships;
+	DEALLOCATE PREPARE stmt_create_term_relationships;
+
+	SET SQL_SAFE_UPDATES=0;
+
+	SET @str_update_counts = CONCAT(
+		'INSERT INTO wp_', @wp_table_infix, 'term_relationships (',
+		' update wp_', @wp_table_infix, 'term_taxonomy tt ',
+			' inner join ( ',
+				' select term_taxonomy_id, count(*) as c ',
+				' from wp_', @wp_table_infix, 'term_relationships ',
+				' group by term_taxonomy_id ',
+			' ) c on tt.term_taxonomy_id = c.term_taxonomy_id ',
+		' set tt.count = c.c; ');
+	PREPARE stmt_update_counts FROM @str_update_counts;
+	EXECUTE stmt_update_counts;
+	DEALLOCATE PREPARE stmt_update_counts;
+
+END
+//
+
+
 DROP PROCEDURE IF EXISTS copy_mt_entries_to_wp_posts //
 
 CREATE PROCEDURE copy_mt_entries_to_wp_posts(mt_blog_id INT, wp_blog_id INT)
@@ -53,7 +153,7 @@ BEGIN
 			'if(entry_status = 2, \'publish\', \'draft\'), ',
 			'if(entry_allow_comments = 1, \'open\', \'closed\'), ',
 			'if(entry_allow_pings = 1, \'open\', \'closed\'),',
-			'entry_basename, ',
+			'replace(entry_basename, \'_\', \'-\'), ',
 			'\'\', ',
 			'\'\', ',
 			'entry_modified_on, ',
